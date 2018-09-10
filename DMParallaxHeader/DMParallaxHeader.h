7cf66cc994aaacea94a325e688e2b2ed0c29e37f
Dominic Miller <Dominic@app-order.com>
2018-09-10 16:03:50 -0700

    Initial Commit

---
+++ DMParallaxHeader/DMParallaxHeader.h
@@ -0,0 +1,19 @@
+//
+//  DMParallaxHeader.h
+//  DMParallaxHeader
+//
+//  Created by Dominic on 9/10/18.
+//  Copyright © 2018 DominicMiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMParallaxHeader.
+FOUNDATION_EXPORT double DMParallaxHeaderVersionNumber;
+
+//! Project version string for DMParallaxHeader.
+FOUNDATION_EXPORT const unsigned char DMParallaxHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMParallaxHeader/PublicHeader.h>
+
+
